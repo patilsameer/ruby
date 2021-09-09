0d667c025f9c8477b53c9e644bbe0728194dcea3
patilsameer <sbpatil29@gmail.com>
2021-09-09 21:02:08 +0530

    adding synthesize2

--- synthesize_prb2.rb
+++ synthesize_prb2.rb
@@ -1,1 +1,11 @@
-Problem 2. Given two arrays books and authors, merge them and print who wrote which book.
+#Problem 2. Given two arrays books and authors, merge them and print who wrote which book.
+books = ["Design as Art", "Anathem", "Shogun"]
+authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]
+  # fill in code that will print:
+  #   Design As Art was written by Bruno Munari
+  #   Anathem was written by Neal Stephenson
+  #   ...
+ result=books.map.with_index {|book, i| "#{book} is in index #{i}"}
+puts result
+result2=books.map.with_index{|book,i| "#{book} was written by #{authors[i]}"}
+puts result2
